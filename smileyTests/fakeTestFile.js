ced37cc6b6179c0cfd363a4783f771446990d95e
Jihin Ip <jim_ip20@hotmail.com>
2018-05-02 17:54:57 +0100

    Reformatting

--- smileyTests/fakeTestFile.js
+++ smileyTests/fakeTestFile.js
@@ -1,9 +1,11 @@
 console.log("fakeTestFile");
 
+it("Fake test should pass", function () {
+    expects(5).toEqual(5);
+});
+
 describe("Fake Method", function(){
-    it("Fake test should pass", function () {
-        expects(5).toEqual(5);
-    });
+
 
     it("Fake test should fail", function () {
         expects(5).toEqual(4);
